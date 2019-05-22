8d6bb10cffe54495f386ecc5a9327d39ad7aefac
甘博文 <ganbowen@baiwang.com>
2019-05-22 20:16:17 +0800

    完成字典转换函数、类型判断函数工具、提出system的mixins

---
+++ src/assets/utils/trans.js
@@ -0,0 +1,85 @@
+/**
+ * 
+ * author ganbowen
+ * description 公共工具函数
+ * created 2019/05/22 11:41:41
+ * 
+ */
+import { isType } from './typeVali'
+import store from '@system/vuex/modules/dictionary'
+const Dic = store.state
+/**
+ * 将data中的code转换为name的工具函数
+ * @param {Object} data 需要转换的对象
+ * @param {Array} transWord 需要转换的字段组成的arr
+ * @param {Object} Dic 整个字典
+ * @return {} 返回转换过字典的数据
+ */
+export function transDictionary(data = [], transWord = []) {
+    for (let word of transWord) {
+        for (let key in data) {
+            let item = data[key]
+
+            if (isType(item, 'object')) {
+                if (item.hasOwnProperty(word)) {
+                    Dic[word].forEach(element => {
+                        if (item[word] === element.code) {
+                            data[key][word] = element.name
+                        }
+                    })
+                }
+                for (let i in item) {
+                    let it = item[i]
+                    if (isType(it, 'object') || isType(it, 'array')) {
+                        transDictionary(it, transWord)
+                    }
+                }
+            }
+
+            if (isType(item, 'array') && item.length > 0) {
+                transDictionary(item, transWord)
+            }
+
+            if (key === word && !isType(item, 'object') && !isType(item, 'array')) {
+                Dic[word].forEach(element => {
+                    if (element.code === item[word]) {
+                        data[word] = element.name
+                    }
+                })
+            }
+        }
+    }
+    return data
+}
+
+/**
+ * change code to name 
+ * @param {String|Number} it 转换的字段的code
+ * @param {String} word 转换的字段
+ * @return {String} 装换后的name
+ */
+export function changeCodeToName(it, word) {
+    for (let key of Dic[word]) {
+        if (it === Dic[word][key].code) {
+            it = Dic[word][key].name
+            break
+        }
+    }
+    return it
+}
+
+/**
+ * change name to code
+ * @param {String} it 转换的字段的name
+ * @param {String} word 转换的字段
+ * @return {String|Number} 装换后的code
+ */
+export function changeNameToCode(it, word) {
+    for (let key of Dic[word]) {
+        if (it === Dic[word][key].name) {
+            it = Dic[word][key].code
+            break
+        }
+    }
+    return it
+}
