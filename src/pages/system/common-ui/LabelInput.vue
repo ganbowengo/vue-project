77c8a71a4c9ddabbd85cfffbd31b27725ab4f7a2
甘博文 <ganbowen@baiwang.com>
2019-05-23 20:39:34 +0800

    开始基础组件的封装

---
+++ src/pages/system/common-ui/LabelInput.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class='label-box'>
+        <span>{{label}}</span>
+        <Input 
+            v-model="value" 
+            :type="type" 
+            :placeholder="placeholder" 
+            :disabled="disabled"
+            :maxlength="maxlength"
+            :minlength="minlength"
+            :clearable="clearable"
+            :readonly="readonly"
+            :size="size"
+            @on-focus="handleFocus" 
+            @on-blur="handleBlur" 
+            @on-change="handleChange" 
+            @input="handleInput" 
+            @on-enter="handleEnter"
+            >
+        </Input>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LabelInput',
+    props: {
+        type: {
+            type: String,
+            default: 'text'
+        },
+        name: {
+            type: String,
+            default: ''
+        },
+        inputValue: {
+            type: [String, Number],
+            default: ''
+        },
+        placeholder: {
+            type: String,
+            default: ''
+        },
+        disabled:{
+            type: Boolean,
+            default: false
+        }
+    },
+    data () {
+        return {}
+    },
+    methods: {
+        handleChange(event) {
+            this.$emit('on-change', event, this.value)
+        },
+        handleFocus(event) {
+            this.$emit('on-focus', event, this.value)
+        },
+        handleBlur(event) {
+            this.$emit('on-blur', event, this.value)
+        },
+        handleEnter(event) {
+            this.$emit('enter', event, this.value)
+        },
+        handleInput(event) {
+            this.$emit('input', event, this.value)
+        }
+    },
+    watch: {
+        inputValue(val) {
+            this.value = val
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/pages/system/common-ui/index.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ * author ganbowen
+ * description 公共基础组件封装入口文件
+ * created 2019/05/23 11:02:05
+ * 
+ */
+import Vue from 'vue'
+import LabelInput from './LabelInput.vue'
+import LabelDatepicker from './LabelDatepicker.vue'
+import LabelSelect from './LabelSelect.vue'
+import LabelTree from './LabelTree.vue'
+
+
+export default function initCommonUi() {
+    Vue.component('LabelInput', LabelInput)
+    Vue.component('LabelDatepicker', LabelDatepicker)
+    Vue.component('LabelSelect', LabelSelect)
+    Vue.component('LabelTree', LabelTree)
+}
