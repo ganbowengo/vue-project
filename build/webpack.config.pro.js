8ea4e48640c7ce9ac97b9c4a4a862e6abb2d5029
ganbowengo <1245960973@qq.com>
2019-05-25 21:26:30 +0800

    优化部分webpack打包配置

--- build/webpack.config.pro.js
+++ build/webpack.config.pro.js
@@ -9,10 +9,10 @@
 
 const merge = require('webpack-merge')
 const baseConfig = require('./webpack.config.base')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-
+const CleanWebpackPlugin = require('clean-webpack-plugin') // 清空打包文件
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') // css分离打包处理
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin // 打包分析工具
+const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin') // 多线程压缩js 打包速度优化
 module.exports = merge(baseConfig, {
     mode: 'production',
     devtool: 'hidden-source-map',
@@ -25,7 +25,7 @@ module.exports = merge(baseConfig, {
                 {
                     loader: 'postcss-loader',
                     options: {
-                        plugins: [require('autoprefixer'),require('cssnano')]
+                        plugins: [require('autoprefixer'), require('cssnano')]
                     }
                 },
                 "sass-loader"
@@ -40,7 +40,7 @@ module.exports = merge(baseConfig, {
         }),
         new CleanWebpackPlugin(),
         new BundleAnalyzerPlugin({
-            openAnalyzer: false
+            openAnalyzer: false // 是否自动打开分析页面
         })
     ],
     optimization: {
@@ -66,6 +66,23 @@ module.exports = merge(baseConfig, {
         },
         runtimeChunk: {
             name: 'manifest'
-        }
+        },
+        // minimizer: [
+        //     // 多线程压缩JS
+        //     new ParallelUglifyPlugin({
+        //         // cacheDir: '.cache/', // 缓存压缩后的结果，下次遇到一样的输入时直接从缓存中获取压缩后的结果并返回
+        //         exclude: /[\\/]node_modules[\\/]/,
+        //         uglifyJS: {
+        //             output: {
+        //                 comments: false, // 是否保留代码中的注释
+        //                 beautify: false // 是否输出可读性较强的代码，即会保留空格和制表符
+        //             },
+        //             compress: {
+        //                 warnings: false,
+        //                 drop_console: true // 是否删除代码中所有的console语句，默认为不删除
+        //             }
+        //         }
+        //     })
+        // ]
     }
 })
